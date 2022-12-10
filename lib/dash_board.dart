c6cdf5729043770de1eff06a0b32ffebbc01acca
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-10 16:14:50 +0800

    Extract fonts files

--- lib/dash_board.dart
+++ lib/dash_board.dart
@@ -11,18 +11,16 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Meal App"),
       ),
-      body: Container(
-        margin: const EdgeInsets.all(15),
-        child: GridView(
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          children: DUMMY_CATEGORIES.map((cat) {
-            return CategoryItem(cat.title, cat.color);
-          }).toList(),
-        ),
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(cat.title, cat.color);
+        }).toList(),
       ),
     );
   }
