722080a7c0fdbdc47c90dcbbff0e9a592218ff49
dondonjedd <70884225+dondonjedd@users.noreply.github.com>
2022-12-12 13:03:08 +0800

    replacing tabs in the appBar to the bottom

--- lib/widgets/tabs_screen.dart
+++ lib/widgets/tabs_screen.dart
@@ -10,25 +10,59 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = const [
+    {"page": Dashboard(), "title": "Categories"},
+    {"page": FavoritesPage(), "title": "Favorites"},
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("Meals"),
-            bottom: const TabBar(tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: "Favorites",
-              ),
-            ]),
-          ),
-          body: const TabBarView(children: [Dashboard(), FavoritesPage()]),
-        ));
+    return Scaffold(
+      appBar:
+          AppBar(title: Text(_pages[_selectedPageIndex]["title"] as String)),
+      body: _pages[_selectedPageIndex]["page"] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: Colors.cyan,
+          currentIndex: _selectedPageIndex,
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                // backgroundColor: Theme.of(context).colorScheme.primary,
+                icon: Icon(
+                  Icons.category_rounded,
+                ),
+                label: "Categories"),
+            BottomNavigationBarItem(icon: Icon(Icons.star), label: "Favorites")
+          ]),
+    );
+    // DefaultTabController(
+    //     length: 2,
+    //     child: Scaffold(
+    //       appBar: AppBar(
+    //         title: const Text("Meals"),
+    //         bottom: const TabBar(tabs: [
+    //           Tab(
+    //             icon: Icon(Icons.category),
+    //             text: "Categories",
+    //           ),
+    //           Tab(
+    //             icon: Icon(Icons.star),
+    //             text: "Favorites",
+    //           ),
+    //         ]),
+    //       ),
+    //       body: const TabBarView(children: [Dashboard(), FavoritesPage()]),
+    //     ));
   }
 }
